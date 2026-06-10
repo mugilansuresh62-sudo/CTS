f46f18b67a576d33c0f2940f18d4a66be91b4f40
mugilansuresh62-sudo <mugilansuresh62@gmail.com>
2026-06-10 11:14:43 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package decompile;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        int sum = a + b;
+
+        System.out.println("Sum = " + sum);
+    }
+}
---
+++ test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public void display() {
+
+        System.out.println("Hello Bytecode");
+    }
+
+    public static void main(String[] args) {
+
+        Test obj = new Test();
+
+        obj.display();
+    }
+}
